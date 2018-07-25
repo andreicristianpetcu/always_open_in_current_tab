a8b0626709405fe8ef226a7f95587a559e86e99d
Andrei Cristian Petcu <andrei@ceata.org>
2018-07-25 22:11:38 +0300

    added several invokations

--- always_open_in_current_tab.js
+++ always_open_in_current_tab.js
@@ -1,1 +1,12 @@
-document.querySelectorAll('a[target="_blank"]').forEach(a => a.target="");
+function removeTarketBlankFromAllLinks(){
+    console.log("removeTarketBlankFromAllLinks");
+    document.querySelectorAll('a[target="_blank"]').forEach(a => a.target="");
+}
+
+setTimeout(function () {
+    removeTarketBlankFromAllLinks();
+}, 300);
+
+document.addEventListener("DOMContentLoaded", function(event){
+    removeTarketBlankFromAllLinks();
+});
